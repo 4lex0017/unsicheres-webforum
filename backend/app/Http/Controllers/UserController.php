26950a404b969a80a6b98e0d2a367335227e24ef
Kern, Alexander <alexander.kern.1@student.fhws.de>
2022-05-20 13:11:48 +0200

    Implemented Json Ressource for User

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -2,21 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Resources\UserResource;
+use App\Http\Resources\UserCollection;
 
 class UserController extends Controller
 {
-    //
 
     public function getAllUsers()
     {
-        return User::all();
+        return new UserCollection(User::all());
     }
 
     public function getUserById($user_id)
     {
-        return User::findUser($user_id);
+        return new UserResource(UserController::findUser($user_id));
     }
 
     public function createUser(Request $user)
@@ -26,13 +28,29 @@ class UserController extends Controller
 
     public function updateUser($user_id, Request $userinput)
     {
-        $user = User::findOrFail($user_id);
+        $user = UserController::findOrFail($user_id);
         $user->update($userinput->all);
-        return $user;
+        return new UserResource($user);
     }
 
     public function authorizeUser($user_id, $password)
     {
+        //TODO:
         return User::authUser($user_id, $password->all);
+    }
+
+
+    public function findOrFail($user_id)
+    {
+        $user = DB::table('users')->where('id', '$user_id');
+        if ($user === null) {
+            return null;
+        }
+        return $user;
+    }
+
+    public function findUser($id)
+    {
+        return DB::table('users')->where('id', '$id');
     }
 }
---
+++ backend/app/Http/Resources/UserResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'user_id' => $this->user_id,
+            'email' => $this->email,
+            'password' => $this->password,
+            'name' => $this->name,
+            'score' => $this->score,
+            'message_count' => $this->message_count,
+            'profile_picture' => $this->profile_picture,
+            'description' => $this->description,
+            'birth_date' => $this->birth_date,
+            'location' => $this->location
+        ];
+    }
+}
