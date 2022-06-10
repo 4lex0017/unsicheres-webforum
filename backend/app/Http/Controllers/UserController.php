26f65247bc377af0db9920346367c82431a688f1
Kern, Alexander <alexander.kern.1@student.fhws.de>
2022-06-10 19:37:26 +0200

    test inject in create

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -26,13 +26,12 @@ class UserController extends Controller
 
     public function getUserById($user_id)
     {
-        //return UserResource::collection(UserController::findUser($user_id));
-
         return new UserResource(UserController::findUser($user_id));
     }
 
     public function createUser(Request $user)
     {
+        // For later return new UserResource(UserController::injectableInsert($user->all()));
         return User::create($user->all());
     }
 
@@ -46,11 +45,11 @@ class UserController extends Controller
         }
     }
 
-    public function authorizeUser($user_id, $password)
-    {
-        //TODO:
-        return User::authUser($user_id, $password->all);
-    }
+    // public function authorizeUser($user_id, $password)
+    // {
+    //     //TODO:
+    //     return User::authUser($user_id, $password->all);
+    // }
 
     public function findUser($user_id)
     {
@@ -62,5 +61,14 @@ class UserController extends Controller
         return DB::connection('insecure')->table('users')->select(
             '*'
         )->whereRaw($row . " = " . $id)->get();
+    }
+
+    public function injectableInsert($userdata)
+    {
+        $user = new User($userdata);
+        return DB::connection('insecure')->insert('insert into users(name, password, birth_date, location, about, groups, endorsements, profile_picture, profile_comments) values ("'
+            . $user->name . '" , "' . $user->password . '" , "' . $user->birth_date . '" , "' . $user->location .
+            '" , "' . $user->about . '" , "' . $user->groups . '" , "' . $user->endorsements . '" , "' . $user->profile_picture .
+            '" , "' . $user->profile_comments . '" )');
     }
 }
