7f1364b0483011a57b82d9e453f03e2b309432c9
Katzenberger, Daniel <daniel.katzenberger@student.fhws.de>
2022-06-22 20:02:30 +0200

    we do need a "global" search too

--- backend/app/Http/Controllers/SearchController.php
+++ backend/app/Http/Controllers/SearchController.php
@@ -8,16 +8,30 @@ use Illuminate\Support\Facades\DB;
 
 class SearchController extends Controller
 {
+    public function globalSearch(Request $request): JsonResponse|array
+    {
+        $q = $request->query('q');
+        if (!$q)
+            return response()->json()->setStatusCode(404);
+
+        $users = self::findUsers($q);
+        $posts = self::findPosts($q);
+        $threads = self::findThreads($q);
+
+        return [
+            'users' => $users,
+            'posts' => $posts,
+            'threads' => $threads
+        ];
+    }
+
     public function searchUsers(Request $request): JsonResponse|array
     {
         $q = $request->query('q');
         if (!$q)
             return response()->json()->setStatusCode(404);
 
-        $users = DB::connection('insecure')->table('users')->select(
-            'id',
-            'name'
-        )->whereRaw('name LIKE \'%' . $q . '%\'')->get();
+        $users = self::findUsers($q);
 
         return [
             'users' => $users
@@ -30,15 +44,7 @@ class SearchController extends Controller
         if (!$q)
             return response()->json()->setStatusCode(404);
 
-        $posts = DB::connection('insecure')->select(DB::raw(
-            'SELECT
-                    p.id,
-                    p.content,
-                    t.id as threadId
-                   FROM posts as p
-                   INNER JOIN threads t on t.id = p.thread_id
-                   WHERE p.content LIKE \'%' . $q . '%\';'
-        ));
+        $posts = self::findPosts($q);
 
         return [
             'posts' => $posts
@@ -51,13 +57,39 @@ class SearchController extends Controller
         if (!$q)
             return response()->json()->setStatusCode(404);
 
-        $threads = DB::connection('insecure')->table('threads')->select(
-            'id',
-            'title'
-        )->whereRaw('title LIKE \'%' . $q . '%\'')->get();
+        $threads = self::findThreads($q);
 
         return [
             'threads' => $threads,
         ];
+    }
+
+    private static function findUsers($q): array
+    {
+        return DB::connection('insecure')->table('users')->select(
+            'id',
+            'name'
+        )->whereRaw('name LIKE \'%' . $q . '%\'')->get()->toArray();
+    }
+
+    private static function findPosts($q): array
+    {
+        return DB::connection('insecure')->select(DB::raw(
+            'SELECT
+                    p.id,
+                    p.content,
+                    t.id as threadId
+                   FROM posts as p
+                   INNER JOIN threads t on t.id = p.thread_id
+                   WHERE p.content LIKE \'%' . $q . '%\';'
+        ));
+    }
+
+    private static function findThreads($q): array
+    {
+        return DB::connection('insecure')->table('threads')->select(
+            'id',
+            'title'
+        )->whereRaw('title LIKE \'%' . $q . '%\'')->get()->toArray();
     }
 }
