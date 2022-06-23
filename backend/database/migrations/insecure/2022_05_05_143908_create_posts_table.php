9931160b93b7fc6c5367aade427877b54f91f814
Katzenberger, Daniel <daniel.katzenberger@student.fhws.de>
2022-06-24 00:47:50 +0200

    cascade to delete all posts of threads

--- backend/database/migrations/insecure/2022_05_05_143908_create_posts_table.php
+++ backend/database/migrations/insecure/2022_05_05_143908_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
         Schema::connection('insecure')->create('posts', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('thread_id');
-            $table->foreign('thread_id')->references('id')->on('threads');
+            $table->foreign('thread_id')->references('id')->on('threads')->cascadeOnDelete();
             $table->unsignedBigInteger('author');
             $table->foreign('author')->references('id')->on('users');
             $table->json('liked_from')->default('[]');
