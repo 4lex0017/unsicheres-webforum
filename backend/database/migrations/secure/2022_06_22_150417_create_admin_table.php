601c97558471785673b611182feb0b35a0fd3c3d
Chris Reichel <chris.reichel@gmx.net>
2022-06-23 11:54:40 +0200

    filter/monitor now read the actual full URI

--- backend/database/migrations/secure/2022_06_22_150417_create_admin_table.php
+++ backend/database/migrations/secure/2022_06_22_150417_create_admin_table.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::connection('secure')->dropIfExists('admin');
+        Schema::connection('secure')->dropIfExists('admins');
     }
 };
