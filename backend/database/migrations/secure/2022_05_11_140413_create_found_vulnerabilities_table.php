a61384b74c62b9f6c093520be787ced91a354207
Chris Reichel <chris.reichel@gmx.net>
2022-06-25 14:20:45 +0200

    scoreboard tracks and returns vulnerability too now

--- backend/database/migrations/secure/2022_05_11_140413_create_found_vulnerabilities_table.php
+++ backend/database/migrations/secure/2022_05_11_140413_create_found_vulnerabilities_table.php
@@ -16,6 +16,8 @@ return new class extends Migration {
             $table->id();
             $table->unsignedBigInteger('attacker_id');
             $table->foreign('attacker_id')->references('attacker_id')->on('attackers');
+            $table->unsignedBigInteger('vulnerability_id');
+            $table->foreign('vulnerability_id')->references('vulnerability_id')->on('vulnerabilities');
             $table->string('vuln_type');
             $table->integer('difficulty');
             $table->timestamps();
