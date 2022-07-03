ae622100bbe956b0b14ba6b0b37a7f74c6efd9e1
Becker, Daniel <daniel.becker@student.fhws.de>
2022-07-04 00:06:27 +0200

    Changed: Reworked config view design

--- frontend/src/app/admin/dialog/dialog-show-current-config/dialog-show-current-config.component.ts
+++ frontend/src/app/admin/dialog/dialog-show-current-config/dialog-show-current-config.component.ts
@@ -1,7 +1,7 @@
 import {ChangeDetectorRef, Component, Inject, OnInit} from '@angular/core';
 import {DifficultyPickerService} from "../../../data-access/services/difficulty-picker.service";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import {VulnerabilitiesConfig} from "../../../data-access/models/vulnerabilitiesConfig";
+import {Config, VulnerabilitiesConfig} from "../../../data-access/models/vulnerabilitiesConfig";
 import {Observable} from "rxjs";
 import {BackendCommunicationService} from "../../../data-access/services/backend-communication.service";
 
@@ -10,22 +10,17 @@ import {BackendCommunicationService} from "../../../data-access/services/backend
   templateUrl: './dialog-show-current-config.component.html',
   styleUrls: ['./dialog-show-current-config.component.scss']
 })
-export class DialogShowCurrentConfigComponent implements OnInit {
+export class DialogShowCurrentConfigComponent {
 
   constructor(
     private changeDetectorRef: ChangeDetectorRef,
     private diffPicker: DifficultyPickerService,
     public dialogRef: MatDialogRef<string>,
     private backendCom: BackendCommunicationService,
-    @Inject(MAT_DIALOG_DATA) public data: VulnerabilitiesConfig,
+    @Inject(MAT_DIALOG_DATA) public data: VulnerabilitiesConfig = {data: [], hash_difficulty: -1},
   ) {
   }
 
-  config$: Observable<VulnerabilitiesConfig>
-
-  ngOnInit(): void {
-    this.config$ = this.backendCom.getVulnerabilitiesConfig()
-  }
 
   close(): void {
     this.dialogRef.close();
