17b7d2c963a02a814969491b570d489ec663d9a9
Katzenberger, Daniel <daniel.katzenberger@student.fhws.de>
2022-07-25 12:35:42 +0200

    force the reset so the script also works in production

--- backend/reset_db.sh
+++ backend/reset_db.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # reset tables
-docker exec php php artisan migrate:reset --path=/database/migrations/secure/ --path=/database/migrations/insecure &&
+docker exec php php artisan migrate:reset --path=/database/migrations/secure/ --path=/database/migrations/insecure --force &&
 
 # migrate them again
-docker exec php php artisan migrate --path=/database/migrations/secure/ --path=/database/migrations/insecure &&
+docker exec php php artisan migrate --path=/database/migrations/secure/ --path=/database/migrations/insecure --force &&
 
 # fill with our seeders
-docker exec php php artisan db:seed
+docker exec php php artisan db:seed --force
